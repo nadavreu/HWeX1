514ec525b350dc10f37b3b48eac7b1dcc0ca592d
nadavreu <36810073+nadavreu@users.noreply.github.com>
2018-03-04 13:22:23 +0200

    HomeWorkEx1fixed

--- HomeWorkEx1/app/src/main/java/com/example/reubens_pc/homeworkex1/MainActivity.java
+++ HomeWorkEx1/app/src/main/java/com/example/reubens_pc/homeworkex1/MainActivity.java
@@ -28,32 +28,40 @@ public class MainActivity extends AppCompatActivity {
         outerLayout = (LinearLayout) findViewById(R.id.outerLayout);
     }
     public void downPart(View v){
-        for (int j = 0; j < 9; j += 3) {
-            for (int i = 0; i < 3; i++) {
-                textViewArray[j + i].setWidth(250);
-                textViewArray[j + i].setHeight(250);
+        EditText matrixNumber=(EditText)findViewById(R.id.matrixNumber);
+        String value= matrixNumber.getText().toString();
+        int matrixNum=Integer.parseInt(value);
+        int textViewCount = matrixNum*matrixNum;
+        for (int j = 0; j < textViewCount; j +=  matrixNum) {
+            for (int i = 0; i < matrixNum; i++) {
+                textViewArray[j + i].setWidth(100);
+                textViewArray[j + i].setHeight(100);
                 textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_off, getTheme()));
-                //for the first diagonal
-                if(j/3 == i) {
+
+                if(j/matrixNum == i) {
                     textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_bold, getTheme()));
                 }
-                // all the cells that's their row j index is bigger then the column index i
-                if(j/3 > i){
+
+                if(j/matrixNum > i){
                     textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_bold, getTheme()));
                 }
             }
         }
     }
     public void upperPart(View v){
-        for (int j = 0; j < 9; j += 3) {
-            for (int i = 0; i < 3; i++) {
-                textViewArray[j + i].setWidth(250);
-                textViewArray[j + i].setHeight(250);
+        EditText matrixNumber=(EditText)findViewById(R.id.matrixNumber);
+        String value= matrixNumber.getText().toString();
+        int matrixNum=Integer.parseInt(value);
+        int textViewCount = matrixNum*matrixNum;
+        for (int j = 0; j < textViewCount; j += matrixNum) {
+            for (int i = 0; i < matrixNum; i++) {
+                textViewArray[j + i].setWidth(100);
+                textViewArray[j + i].setHeight(100);
                 textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_off, getTheme()));
-                if(j/3 == i) {
+                if(j/matrixNum == i) {
                     textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_bold, getTheme()));
                 }
-                if(i > j/3){
+                if(i > j/matrixNum){
                     textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_bold, getTheme()));
                 }
             }
@@ -61,30 +69,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void diagonals(View v){
-        for (int j = 0; j < 9; j += 3) {
-            for (int i = 0; i < 3; i++) {
-                textViewArray[j + i].setWidth(250);
-                textViewArray[j + i].setHeight(250);
+        EditText matrixNumber=(EditText)findViewById(R.id.matrixNumber);
+        String value= matrixNumber.getText().toString();
+        int matrixNum=Integer.parseInt(value);
+        int textViewCount = matrixNum*matrixNum;
+        for (int j = 0; j < textViewCount; j += matrixNum) {
+            for (int i = 0; i < matrixNum; i++) {
+                textViewArray[j + i].setWidth(100);
+                textViewArray[j + i].setHeight(100);
                 textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_off, getTheme()));
 
-                if(j/3 == i) {
+                if(j/matrixNum == i) {
                     textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_bold, getTheme()));
                 }
 
-                if(j/3 + i == 2) {
+                if(j/matrixNum + i == matrixNum-1) {
                     textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_bold, getTheme()));
                 }
             }
         }
     }
     public void border(View v){
-        for (int j = 0; j < 9; j += 3) {
-            for (int i = 0; i < 3; i++) {
-                textViewArray[j + i].setWidth(250);
-                textViewArray[j + i].setHeight(250);
+        EditText matrixNumber=(EditText)findViewById(R.id.matrixNumber);
+        String value= matrixNumber.getText().toString();
+        int matrixNum=Integer.parseInt(value);
+        int textViewCount = matrixNum*matrixNum;
+        for (int j = 0; j < textViewCount; j += matrixNum) {
+            for (int i = 0; i < matrixNum; i++) {
+                textViewArray[j + i].setWidth(100);
+                textViewArray[j + i].setHeight(100);
                 textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_off, getTheme()));
 
-                if(j == 0 || j/3 == 2 || i == 0 || i == 2) {
+                if(j == 0 || j/matrixNum == matrixNum-1 || i == 0 || i == matrixNum-1) {
                     textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_bold, getTheme()));
                 }
             }
@@ -109,26 +125,27 @@ public class MainActivity extends AppCompatActivity {
                         LinearLayout.LayoutParams.WRAP_CONTENT,
                         LinearLayout.LayoutParams.WRAP_CONTENT);
                 layoutParams.gravity = Gravity.CENTER;
-                linLayoutRows[i].setLayoutParams(layoutParams); // importent!!! remember it
+                linLayoutRows[i].setLayoutParams(layoutParams);
                 outerLayout.addView(linLayoutRows[i]);
             }
 
-            // creating a TextView array with 9 cells
-            int textViewCount = 9;
+
+            int textViewCount = matrixNum*matrixNum;
             textViewArray = new TextView[textViewCount];
             for (int i = 0; i < textViewCount; i++) {
                 textViewArray[i] = new TextView(this);
             }
 
-            for (int j = 0; j < 9; j += 3) {
-                for (int i = 0; i < 3; i++) {
-                    textViewArray[j + i].setWidth(250);
-                    textViewArray[j + i].setHeight(250);
+            for (int j = 0; j < textViewCount; j += matrixNum) {
+                for (int i = 0; i < matrixNum; i++) {
+                    textViewArray[j + i].setWidth(100);
+                    textViewArray[j + i].setHeight(100);
                     textViewArray[j + i].setBackground(getResources().getDrawable(R.drawable.button_off, getTheme()));
-                    linLayoutRows[j / 3].addView(textViewArray[j + i]);
+                    linLayoutRows[j / matrixNum].addView(textViewArray[j + i]);
                 }
             }
-
+            setMatrix=(Button)findViewById(R.id.setMatrix);
+            setMatrix.setEnabled(false);
 
 
 
